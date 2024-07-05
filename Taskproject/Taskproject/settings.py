7a0c20da1bc1400b9ce5bb4db45115bca865f9cf
Jhonneto003 <ekpeubonabasi@gmail.com>
2024-07-04 20:30:39 -0700

    added:deployment changes

--- Taskproject/Taskproject/settings.py
+++ Taskproject/Taskproject/settings.py
@@ -11,21 +11,34 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+import dj_database_url
+
+
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_r8%$7ka63s=e-pp%1r1bin)ncs8sa3%9$f)qt6pbryuax)(^z'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*vercel.app']
 
 
 # Application definition
@@ -74,11 +87,11 @@ WSGI_APPLICATION = 'Taskproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':  dj_database_url.parse(env('DATABASE_URL'))
+ 
+    
 }
 
 
